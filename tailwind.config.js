560232fd2243f42359d3958a0360bbb384691813
Asad777 <muhammadasad7.7.2007@gmail.com>
2024-12-02 01:22:18 +0500

    some sample code

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // Watch all HTML files in the current directory
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
